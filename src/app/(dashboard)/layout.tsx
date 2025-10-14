1f53d3dc273b5db450a6f55eb6aa8f919fbb3ecd
SadafNemani <sedefnemani@gmail.com>
2025-10-14 10:10:43 +0330

    implement routing structure with dashboard layout

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import AppSidebar from "@/components/ui/AppSidebar";
+import { Navbar } from "@/components/ui/navbar";
+import { cn } from "@/lib/utils";
+
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div
+    className={cn(
+        "flex w-full h-screen bg-background text-neutral-200 border border-neutral-800 rounded-md"
+    )}
+    >
+    <AppSidebar />
+    <div className="flex-col flex-1">
+        <Navbar />
+        <main className="flex-1 overflow-y-auto bg-background-light">{children}</main>
+    </div>
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,10 @@
 import "./globals.css";
-import React from "react";
-import AppSidebar from "@/components/ui/AppSidebar";
-import { Navbar } from "@/components/ui/navbar";
-import { cn } from "@/lib/utils";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body
-        className={cn(
-          "flex w-full h-screen bg-background text-neutral-200 border border-neutral-800 rounded-md"
-        )}
-      >
-        <AppSidebar />
-        <div className="flex-col flex-1">
-          <Navbar />
-          <main className="flex-1 overflow-y-auto bg-background-light">{children}</main>
-        </div>
+      <body>
+        {children}
       </body>
     </html>
   );
